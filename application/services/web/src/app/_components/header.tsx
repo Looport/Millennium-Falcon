19b1b031744a908101da415966e92cdcbcbbc612
dormammun <khoteiv.dev@gmail.com>
2023-08-24 13:24:49 +0300

    Implement Header Markup

---
+++ application/services/web/src/app/_components/header.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import Image from 'next/image'
+import {IconContext} from 'react-icons'
+import {AiOutlineGift} from 'react-icons/ai'
+import {BiUser} from 'react-icons/bi'
+import {BsTelephoneOutbound} from 'react-icons/bs'
+import {LuSettings} from 'react-icons/lu'
+
+export const Header = () => {
+  console.log('Header')
+
+  return (
+    <header
+      className="
+      flex justify-between items-center mt-[40px]
+      font-bold
+    "
+    >
+      <div className="flex">
+        <Image
+          width={130}
+          height={35}
+          src="/logo.png"
+          alt="Logo"
+        />
+        <div
+          className="
+          h-[35px] w-[2px] ml-[40px]
+          bg-gray-50/25 
+        "
+        />
+      </div>
+      <nav>
+        <ul className="flex gap-[40px]">
+          <li>About Us</li>
+          <li>Rouad Map</li>
+          <li>Donate</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+      <div className="flex gap-[40px]">
+        <IconContext.Provider value={{size: '25px'}}>
+          <button
+            className="
+            relative
+            after:absolute after:top-[1px] after:right-[0px]
+            after:content-[' '] after:w-[6px] after:h-[6px] after:rounded-[50%]
+          after:bg-red-600
+          "
+          >
+            <AiOutlineGift />
+          </button>
+          <button>
+            <LuSettings />
+          </button>
+        </IconContext.Provider>
+      </div>
+      <div className="flex gap-[40px] items-center">
+        <IconContext.Provider value={{size: '20px'}}>
+          <button
+            className="
+            flex gap-[13px] items-center
+            py-[13px] px-[30px] rounded-[50px] 
+            border-2 border-slate-50/25
+          "
+          >
+            <BsTelephoneOutbound />
+            <span>Join a Call</span>
+          </button>
+          <a
+            className="
+            flex gap-[13px] items-center
+            py-[13px] px-[30px] rounded-[50px] 
+            border-2 border-slate-50/25 bg-blue-500
+          "
+            href="/signin"
+          >
+            <BiUser />
+            <span>Sign In</span>
+          </a>
+        </IconContext.Provider>
+      </div>
+    </header>
+  )
+}
--- application/services/web/src/app/page.tsx
+++ application/services/web/src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from 'next/image'
+import {Header} from '@/app/_components/header'
 
 export default function Home() {
   return (
-    <main
-      className="
-      flex min-h-screen 
-      flex-col items-center justify-between p-24
-      "
-    >
-      <h1>Web</h1>
+    <main className="w-3/4 mx-auto">
+      <Header />
     </main>
   )
 }
