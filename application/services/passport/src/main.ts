55d3145227e44d29bd5e1ab256ec29a3d0cdf75d
dormammun <khoteiv.dev@gmail.com>
2023-09-09 00:32:13 +0300

    Create a bash script that organizes docker logic

--- application/services/passport/src/main.ts
+++ application/services/passport/src/main.ts
@@ -3,13 +3,15 @@ import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify'
 
 import {AppModule} from '@/app/app.module'
 
+const EXPOSED_IP = '0.0.0.0';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   )
 
-  await app.listen(3000, '0.0.0.0')
+  await app.listen(3000, EXPOSED_IP)
 }
 
 bootstrap()
--- infrastructure/images/passport/buildx.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --push \
---platform linux/arm64,linux/amd64 \
--t dormammun/looport-passport:latest \
--f ./Dockerfile ../../..
