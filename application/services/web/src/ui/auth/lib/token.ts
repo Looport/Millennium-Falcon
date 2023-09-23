7b65e86dfbb88fca165ee0354aa787f0442225e8
dormammun <khoteiv.dev@gmail.com>
2023-09-23 16:18:25 +0300

    Add token helper

--- application/services/web/src/ui/auth/lib/token.ts
+++ application/services/web/src/ui/auth/lib/token.ts
@@ -1,9 +1,10 @@
 'use client'
 
 export const setToken = (token: string) => {
-  document.cookie = `accessToken=${token};`
+  document.cookie =
+    `accessToken=${token}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
 }
 
 export const cleanToken = () => {
-  document.cookie = `accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`
+  document.cookie = `accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
 }
