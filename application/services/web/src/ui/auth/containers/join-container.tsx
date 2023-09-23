318f4da40dd5998b57d713c2befc96ca8f783336
dormammun <khoteiv.dev@gmail.com>
2023-09-23 16:18:25 +0300

    Add token helper

--- application/services/web/src/ui/auth/containers/join-container.tsx
+++ application/services/web/src/ui/auth/containers/join-container.tsx
@@ -4,6 +4,7 @@ import {login} from '@/network/passport/requests/login.request'
 import {register} from '@/network/passport/requests/register.request'
 import {JoinForm, JoinFormVariantEnum} from '@/ui/auth/components/join-form'
 import {useRefreshPage} from '@/ui/common/hooks/refresh-page'
+import {setToken} from "@/ui/auth/lib/token";
 
 export const JoinContainer = () => {
   const {refresh} = useRefreshPage()
@@ -12,7 +13,7 @@ export const JoinContainer = () => {
     const action = {login, register}[variant]
     const body = await action(data)
 
-    document.cookie = `accessToken=${body.accessToken};`
+    setToken(body.accessToken)
 
     refresh({redirectTo: '/'})
   }
