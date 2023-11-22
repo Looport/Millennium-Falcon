59f8fea42aa53033079998379b6e83744fd4fec1
dormammun <khoteiv.dev@gmail.com>
2023-11-23 00:11:18 +0200

    Fixed ingress call from cluster service

--- application/services/web/src/common/lib/api/get-api-url.ts
+++ application/services/web/src/common/lib/api/get-api-url.ts
@@ -2,7 +2,4 @@
 export const API_HOST =
   process.env.NEXT_PUBLIC_API_HOST ?? '__REPLACE__NEXT_PUBLIC_API_HOST__'
 
-export const INGRESS_API_URL =
-  'ingress-nginx-controller.ingress-nginx.svc.cluster.local'
-
 export const getApiUrl = () => `http://${API_HOST}/api`
--- application/services/web/src/ui/common/lib/headers.server.ts
+++ application/services/web/src/ui/common/lib/headers.server.ts
@@ -2,11 +2,13 @@ import {headers as getNextHeaders} from 'next/headers'
 
 import {RequestOptions} from '@/common/lib/request/utils/request-options.intefrace'
 
+const FROM_HEADERS = ['content-length', 'content-type']
+
 export const getServerHeaders = (): RequestOptions['headers'] => ({
   ...Object.keys(Object.fromEntries(getNextHeaders().entries())).reduce(
     (headers, headerKey) => ({
       ...headers,
-      ...(['content-length', 'content-type'].includes(headerKey)
+      ...(FROM_HEADERS.includes(headerKey)
         ? {}
         : {[headerKey]: getNextHeaders().get(headerKey)}),
     }),
