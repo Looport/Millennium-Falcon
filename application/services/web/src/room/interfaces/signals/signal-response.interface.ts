18b2633ad4edb5d14db5c7fbdca5ee5d4ac4828d
dormammun <khoteiv.dev@gmail.com>
2023-12-08 10:44:15 +0200

    Removed TODO

--- application/services/web/src/room/interfaces/signals/signal-response.interface.ts
+++ application/services/web/src/room/interfaces/signals/signal-response.interface.ts
@@ -1,5 +1,4 @@
 import {JoinSignalResponse} from '@/room/interfaces/signals/join-signal-response.interface'
 import {OfferSignalResponse} from '@/room/interfaces/signals/offer-signal-response.interface'
 
-// TODO: divide response from siganl
 export type SignalResponse = JoinSignalResponse | OfferSignalResponse
